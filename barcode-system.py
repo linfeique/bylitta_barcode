419a92d989e798545efd2b13a6386ddfe2ea937c
Matheus <mcustodio@eusou.space>
2021-10-28 19:59:37 -0300

    added - verification of directories changed

--- barcode-system.py
+++ barcode-system.py
@@ -1,38 +1,48 @@
 from barcode import writer
+from barcode import EAN13
 import shortuuid
 import csv
-from barcode import EAN13
 from barcode.writer import ImageWriter
-from pathlib import Path
 import os
 
-cloth_name = input("Qual o nome do conjunto?\n")
+cloth_name = input("Qual o nome da peça?\n")
 
-qtd_colors = input("Quantas cores esse produtos tem?\n")
-qtd_sizes = input("Quantos tamanhos esse produto tem?\n")
+qtd_colors = input("Quantas cores essa peça tem?\n")
+qtd_sizes = input("Quantos tamanhos essa peça tem?\n")
 
 x = 1 * int(qtd_colors) * int(qtd_sizes)
 
-if os.path.exists("/files"):
-    file = open("files/file.csv", "w", encoding='UTF8')        
-else:
-    os.mkdir("files")
-    file = open("files/file.csv", "w", encoding='UTF8')
+file = NotImplemented
 
-if not os.path.exists("/images"):
-    os.mkdir("images")
+if os.path.isdir("files"):
+	file = open("files/file.csv", "w", encoding='UTF8')
+else:
+	os.mkdir("files")
+	file = open("files/file.csv", "w", encoding='UTF8')
+
+if not os.path.isdir("images"):
+	os.mkdir("images")
+else:
+  pass
 
 for x in range(0, x):
-    numbers = shortuuid.ShortUUID(alphabet="1234567890").random(length=12)
+	numbers = shortuuid.ShortUUID(alphabet="1234567890").random(length=12)
 
-    writer = csv.writer(file)
+	writer = csv.writer(file)
 
-    header = ["Nome", "Número Imagem", "Código de barras"]
-    data = [cloth_name, x, numbers]
+	data = [cloth_name, x, numbers]
 
-    writer.writerow(data)
+	writer.writerow(data)
 
-    barcode = EAN13(numbers, writer=ImageWriter())
-    barcode.save(f"images/{cloth_name}_{x}_codigo")
+	if not os.path.isdir(f"images/{cloth_name}"):
+		os.mkdir(f"images/{cloth_name}")
+	else:
+			pass
+
+	with open(f"images/{cloth_name}/{cloth_name}_{x}_codigo.jpeg", "wb") as f:
+		EAN13(numbers, writer=ImageWriter()).write(f)
+
+	# teste = barcode.get('ean13', numbers, writer=ImageWriter())
+	# teste.save(f"images/{cloth_name}/{cloth_name}_{x}_codigo")
 
 file.close()
